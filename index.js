d68bd45d4a6d54df0a8c60606d1996f927b8c71a
bisheshwarpandey6642 <61419944+bisheshwarpandey6642@users.noreply.github.com>
2024-07-03 20:53:31 +0530

    Update index.js

--- index.js
+++ index.js
@@ -1,9 +1,10 @@
 const express = require("express")
 const bodyparser = require("body-parser")
-const mongoose =require('mongoose')
 const app = express()
 app.use('/display', (req, res) => {
     console.log("Just got a request!")
     res.send('Yo!')
 })
-app.listen(process.env.PORT || 3000)
+app.listen(process.env.PORT || 3000 , () => {
+    console.log("test run node")
+})
